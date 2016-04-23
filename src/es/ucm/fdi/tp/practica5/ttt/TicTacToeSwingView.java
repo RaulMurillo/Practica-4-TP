8bbea0bb6d7d56261e292e791c19618c7e8e7d99
RaulMurillo <ramuri01@ucm.es>
2016-04-23 17:50:46 +0200

    Corregida cabecera de TTT

--- src/es/ucm/fdi/tp/practica5/ttt/TicTacToeSwingView.java
+++ src/es/ucm/fdi/tp/practica5/ttt/TicTacToeSwingView.java
@@ -1,26 +1,29 @@
 package es.ucm.fdi.tp.practica5.ttt;
 
+import java.util.List;
+
 import es.ucm.fdi.tp.basecode.bgame.control.Controller;
 import es.ucm.fdi.tp.basecode.bgame.control.Player;
+import es.ucm.fdi.tp.basecode.bgame.model.Board;
 import es.ucm.fdi.tp.basecode.bgame.model.GameObserver;
 import es.ucm.fdi.tp.basecode.bgame.model.Observable;
 import es.ucm.fdi.tp.basecode.bgame.model.Piece;
-import es.ucm.fdi.tp.practica5.connectn.ConnectNFactoryExt;
 import es.ucm.fdi.tp.practica5.connectn.ConnectNSwingView;
 
-public class TicTacToeFactoryExt extends ConnectNFactoryExt{
+public class TicTacToeSwingView extends ConnectNSwingView{
+
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
 
-	public TicTacToeFactoryExt(){
-		super(3);
+	public TicTacToeSwingView(Observable<GameObserver> g, Controller c, Piece p, Player random, Player ai) {
+		super(g, c, p, random, ai);
 	}
 	
 	@Override
-	public void createSwingView(final Observable<GameObserver> game, final Controller ctrl, final Piece viewPiece,
-			Player randPlayer, Player aiPlayer) {
-		new TicTacToeSwingView(game, ctrl, viewPiece, randPlayer, aiPlayer);
+	public void onGameStart(Board board, String gameDesc, List<Piece> pieces, Piece turn) {
+		super.onGameStart(board, "Tic-Tac-Toe", pieces, turn);
 	}
+
 }
